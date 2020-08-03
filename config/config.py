2c4e058611935b73191ef06753e1c02961bbd727
cattree <morestarforyou@gmail.com>
2020-08-03 16:56:31 +0800

    Update config.py
    更新路径

--- config/config.py
+++ config/config.py
@@ -1,9 +1,14 @@
-class Config:
-    x_scaler_path = '/home/ubuntu/PycharmProjects/fog_benchmark/model/x_scaler.pkl'
-    y_scaler_path = '/home/ubuntu/PycharmProjects/fog_benchmark/model/y_scaler.pkl'
+import os
 
-    predict_model_path = '/home/ubuntu/PycharmProjects/fog_benchmark/model/mse-青岛优化 全-3-4-25-244(4e).h5'
-    data_path = '/home/ubuntu/PycharmProjects/fog_benchmark/data/xxx.csv'
+
+class Config:
+    path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+    x_scaler_path = os.path.join(path, 'model/x_scaler.pkl')
+    y_scaler_path = os.path.join(path, 'model/y_scaler.pkl')
+
+    predict_model_path = os.path.join(path, 'model/mse-青岛全 预报2H-3-4-25-257(4e).h5')
+    data_path = os.path.join(path, 'data/xxx.csv')
 
     data_index = 'time'
 
