79e7fc1ea6a115336079e1bfcc1fb3ce5610cd1a
cattree <morestarforyou@gmail.com>
2020-08-03 16:56:17 +0800

    Update main.py
    更新当GPU参与时出现的bug

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import os
 from numpy.random import seed
 from benchmark.benchmark import Benchmark
+import tensorflow as tf
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+tf.config.experimental.set_virtual_device_configuration(gpus[0], [
+    tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
 
 if __name__ == '__main__':
     kilometre = 5000
